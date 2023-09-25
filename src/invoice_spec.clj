d13b6b5b331e4b77ff03b53dbf70bd0d5d41fba2
Carlos Martinez Camelo <cemartinezca@gmail.com>
2023-09-25 16:58:00 -0500

    Problem 2: Generating invoice based on the json file

--- src/invoice_spec.clj
+++ src/invoice_spec.clj
@@ -1,6 +1,9 @@
 (ns invoice-spec
   (:require
-    [clojure.spec.alpha :as s]))
+    [clojure.spec.alpha :as s]
+    [clojure.data.json :as json] 
+    [clojure.string :as str] 
+    [clj-time.format :as f]))
 
 (defn not-blank? [value] (-> value clojure.string/blank? not))
 (defn non-empty-string? [x] (and (string? x) (not-blank? x)))
@@ -54,3 +57,68 @@
        (filter is-valid-item?)))
 
 (println invoice-data)
+
+;; Problem 2: Core Generating Functions
+
+(defn get-value
+  [key value]
+  (def time-keys ["issue_date" "payment_date"])
+  
+  (cond 
+    (some #{key} time-keys) (let [source-formatter (f/formatter "dd/MM/yyyy")
+                                  dest-formatter (f/formatter "yyyy-MM-dd")
+                                  parsed-date (f/parse source-formatter value)
+                                  dest-date (f/unparse dest-formatter parsed-date)
+                                  result (clojure.instant/read-instant-date dest-date)]
+                              result)
+    (= key "items") (let [invoice-items (map 
+                                         #(reduce (fn [new-map [k v]]
+                                                    ;;(assoc new-map (keyword "invoice-item" k) v))
+                                                    (if (= k "taxes")
+                                                      (assoc new-map (keyword "invoice-item" k) (get-value k v))
+                                                      (assoc new-map (keyword "invoice-item" k) v)))
+                                                  {}
+                                                  %) value)
+                          result (into [] invoice-items)]
+                      result)
+    (= key "taxes") (let [taxes (map #(let [category :iva
+                                            rate (get % "tax_rate")]
+                                        {(keyword "tax" "category") category
+                                         (keyword "tax" "rate") (double rate)}) value)
+                                ;;  #(reduce (fn [new-map [k v]]
+                                ;;             (assoc new-map (keyword "tax" (str/replace k #"tax_" "")) v))
+                                ;;           {}
+                                ;;           %) value)
+                          result (into [] taxes)]
+                      result)
+    (= key "customer") (let [name (get value "company_name")
+                             email (get value "email")
+                             result {:customer/name name :customer/email email}]
+                         result)
+    :else  value))
+
+;; (inst? (get-value "payment_date" "12/11/2020"))
+
+(defn organize-map
+  ([m] (organize-map {} [] m))
+  ([result-map path m]
+   (reduce (fn [new-map [key val]]
+             (let [new-path (conj path (str/replace key #"_" "-"))
+                   key-word (keyword (first new-path) (clojure.string/join "/" (rest new-path)))
+                   final-value (get-value key val)]
+               (cond
+                 (= key "customer") (assoc new-map key-word final-value)
+                 (map? val) (organize-map new-map new-path val)
+                 :else (assoc new-map key-word final-value))))
+           result-map
+           m)))
+
+(def invoice-json (-> (slurp "invoice.json")
+                      (json/read-str)
+                      (organize-map)))
+
+;; (organize-map (json/read-str (slurp "invoice.json")))
+
+
+(s/explain ::invoice invoice-json)
+(s/valid? ::invoice invoice-json)
